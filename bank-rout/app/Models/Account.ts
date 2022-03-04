a7bb0e8f4a54854b4033c1751fa6d33d27607486
ReneRugaba <rugabarj@gmail.com>
2022-03-04 10:37:36 +0100

    add entity account

---
+++ bank-rout/app/Models/Account.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Account extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public accountNb:string
+
+  @column()
+  public balance:number
+
+  @column()
+  public overdraf:number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static accountNb(account: Account){
+    account.accountNb='FR'+account.id + Math.random().toString(36)
+  }
+}
