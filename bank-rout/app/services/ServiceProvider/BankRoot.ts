e7475ea21df59cac0097ae22a3e47d90d634a804
mocquaischristopher <73651871+mocquaischristopher@users.noreply.github.com>
2022-03-07 10:02:51 +0100

    update merge

--- bank-rout/app/services/ServiceProvider/BankRoot.ts
+++ bank-rout/app/services/ServiceProvider/BankRoot.ts
@@ -6,29 +6,28 @@ export default class BankRootService implements BankrootInteface {
 
     getAllCustomer=async()=>
     {
-        await this.creatFakeCustomer()
         return await Customer.all()
     }
 
-    creatFakeCustomer=async()=>{
+    // creatFakeCustomer=async()=>{
        
 
-        for(let i=1;i<=10;i++){
-            let object =new Customer()
-                object.firstname=`nom${i}`,
-                object.lastname= `prenom${i}`,
-                object.email=`email${i}@mail.com`,
-                object.password='testMot2pass',
-                object.telnumber=`5444646464`
+    //     for(let i=1;i<=10;i++){
+    //         let object =new Customer()
+    //             object.firstname=`nom${i}`,
+    //             object.lastname= `prenom${i}`,
+    //             object.email=`email${i}@mail.com`,
+    //             object.password='testMot2pass',
+    //             object.telnumber=`5444646464`
             
-                Customer.create({
-                    firstname:object.firstname,
-                    lastname:object.lastname,
-                    email:object.email,
-                    password:object.password,
-                    telnumber:object.telnumber
-                })
-        }
+    //             Customer.create({
+    //                 firstname:object.firstname,
+    //                 lastname:object.lastname,
+    //                 email:object.email,
+    //                 password:object.password,
+    //                 telnumber:object.telnumber
+    //             })
+    //     }
         
-    }
+    // }
 }
