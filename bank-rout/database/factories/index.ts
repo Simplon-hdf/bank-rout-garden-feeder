ed9b2c972ea05656ea26806677fe4b59dc061d4b
mocquaischristopher <73651871+mocquaischristopher@users.noreply.github.com>
2022-03-09 10:39:50 +0100

    update factory/index.ts no utilisation faker

--- bank-rout/database/factories/index.ts
+++ bank-rout/database/factories/index.ts
@@ -1,10 +1,2 @@
-import Transfer from 'App/Models/Transfer'
-import Factory from '@ioc:Adonis/Lucid/Factory'
+// import Factory, { FactoryContextContract } from '@ioc:Adonis/Lucid/Factory'
 
-export const TransferFactory = Factory
-    .define(Transfer, ({ faker }) => {
-        return {
-            numberTransfer: faker.numberTransfer(),
-        }
-    })
-    .build()
